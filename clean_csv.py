d4b091744b3fb8d299a270676d1da4a5cf001bda
souya taniguchi <sou.tan@outlook.com>
2023-08-24 17:52:58 +0900

    csvファイルをテキストクリーニング

--- clean_csv.py
+++ clean_csv.py
@@ -17,3 +17,20 @@ if __name__ == "__main__":
     input_file = "an.csv"  # 元のCSVファイル名を指定
     output_file = "an_clean.csv"  # クリーニングした結果を書き込むCSVファイル名を指定
     remove_tabs(input_file, output_file)
+    
+#csvファイルをカテゴリー分け
+import pandas as pd
+
+# CSVファイルを読み込む（エンコーディング指定）
+csv_file_path = 'an_clean.csv'
+data = pd.read_csv(csv_file_path, encoding='shift-jis')
+print(data.head())
+
+# カテゴリーと対応するキーワードの辞書を作成
+category_keywords = {
+    '恋愛関係': ['彼女', '振られ', '親'],
+    '友人関係': ['友達', '嫌われ', '嫌い'],
+    '職場関係': ['アルバイト', '上司', 'ストレス'],
+    # 他のカテゴリーも同様に定義
+}
+
